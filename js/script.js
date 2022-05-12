2268a03ae1a2d6c9480026353cdc44f756efcc37
unknown <markocvetinovic030@gmail.com>
2022-05-12 15:49:15 +0200

    third commit

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 let myNav = document.querySelector("header");
-let p = document.querySelector("p")
+
+
 
 window.onscroll = function () {
-    "use strict";
     if (document.body.scrollTop >= 1 || document.documentElement.scrollTop >= 1) {
-        myNav.style.backgroundColor = 'white';
-        myNav.style.transition = '0.8s all';
-        p.style.color = 'black';
-
+        myNav.style.backgroundColor = 'black';
+        myNav.style.transition = '0.5s all';
+        
 
     } else {
         myNav.style.backgroundColor = 'transparent';
-        p.style.color = 'white';
+        a.style.transition = '0.5s all';
+        
     }
 };
